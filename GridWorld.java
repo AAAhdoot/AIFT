9aeb46d92715bb1fb8adffb08eaf099a920f8a2c
Ashni Mehta <ashnimehta@gmail.com>
2017-02-18 18:56:55 -0500

    Adding blocked/unblocked to grids

--- GridWorld.java
+++ GridWorld.java
@@ -8,10 +8,19 @@ public class GridWorld{
 	}
 
 	public void populate(){
+		int chance = 0;
 		for(int i=0;i<101;i++){
 			for(int j=0;j<101;j++){
+				this.grid[i][j].isBlocked = false;
 				this.grid[i][j].x_coordinate = i;
 				this.grid[i][j].y_coordinate = j;
+				// with 30% probability mark unblocked
+				// with 70% probability mark unblocked
+				chance = (int)(Math.random() * 101);
+				if(chance <= 30){
+					this.grid[i][j].isBlocked = true;
+				}
+				chance = 0;
 			}
 		}
 		return;
