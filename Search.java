41ac6d421ff24cddffab73fb5ee10db5d0b12cf6
Ashni Mehta <ashnimehta@gmail.com>
2017-02-18 22:47:00 -0500

    Command line output for timing

--- Search.java
+++ Search.java
@@ -8,6 +8,32 @@ public class Search{
 			// System.out.println(i);
 		}
 
+		System.out.println("Comparing Repeated Forward A* with ties");
+		//run repeated forward a * where small g favored & time
+		//run repeated forward a * where large g favored & time
+		System.out.println("Average time to find target in mazes:");
+		System.out.println("Repeated Forward A* where small G favored: ");
+		System.out.println("Repeated Forward A* where large G favored: ");
+		System.out.println();
+		System.out.println();
+		System.out.println("Forward vs Backward Repeated A*");
+		//run forward a * and time
+		//run backward a* and time
+		System.out.println("Average time to find target in mazes:");
+		System.out.println("Repeated Forward A* : ");
+		System.out.println("Repeated Backward A* : ");
+		System.out.println();
+		System.out.println();
+		System.out.println("Repeated Forward vs Adaptive.");
+		//run repeated forward a*
+		//run adaptive a*
+		System.out.println("Average time to find target in mazes:");
+		System.out.println("Repeated Forward A* : ");
+		System.out.println("Adaptive A* : ");
+		System.out.println();
+		System.out.println();
+
+
 		//GridWorld temp = new GridWorld();
 		return;
 	}
